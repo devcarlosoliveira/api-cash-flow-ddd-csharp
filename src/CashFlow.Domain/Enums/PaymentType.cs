da04e6b6a97ebd500d7f4033d0604497b73d8c2c
Carlos Oliveira <68062585+devcarlosoliveira@users.noreply.github.com>
2024-08-14 11:44:13 -0300

    add EntityFramework

---
+++ src/CashFlow.Domain/Enums/PaymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Domain.Enums;
+
+public enum PaymentType
+{
+    None = 0,
+    Cash = 1,
+    CreditCard = 2,
+    DebitCard = 3,
+    EletronicTransfer = 4,
+}
---
+++ src/CashFlow.Infrastructure/EntityFramework/ClashFlowDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.EntityFramework;
+
+public class ClashFlowDbContext : DbContext
+{
+    public ClashFlowDbContext() { }
+
+    public ClashFlowDbContext(DbContextOptions<ClashFlowDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "DataSource=cashflow.db;Cache=Shared";
+        optionsBuilder.UseSqlite(connectionString);
+    }
+
+    public DbSet<Expense> Expenses { get; set; }
+
+    #region Required
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //}
+    #endregion
+}
